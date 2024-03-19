a090e995782dccf918a486f24a7e188861caf982
mattolbert <163608710+mattolbert@users.noreply.github.com>
2024-03-19 14:19:42 -0400

    3-19 update
    added combination function and other things. didn't break it down into new branch first, probably should have, but i'm still working on my work laptop. tomorrow would be good day to walk through installing what i need on home laptop to code there.

--- ShutTheBox.py
+++ ShutTheBox.py
@@ -1,49 +1,48 @@
 import random
-
-#nums = list(range(1, 13))
-#def roll():
-  #count = 0
-  #while nums:
-    #die1 = random.randint(1, 6)
-   # die2 = random.randint(1, 6)
-    #rollvalue = die1 + die2
-    #print(nums)
-    #print("First die: "+ str(die1) + ", Second die: " + str(die2) + ", #Total: " + str(rollvalue) + "\n")
-    #if rollvalue in nums:
-     # nums.remove(rollvalue)
-      #count += 1
-    #else:
-      #print(nums, rollvalue)
-     # print("You lost! You rolled " + str(count) + " different numbers in a row.")
-      #break
-  #score = ''.join(map(str, nums))
-  #print("Your final score is " + score)
- 
-  #return rollvalue, nums
-#roll()
+from itertools import combinations
 
 nums = list(range(1, 13))
 print(nums)
+
+#determine if any combo of remaining numbers adds up to the dice roll. To be fair, I ended up googling how to do this, I don't know if I could have figured out a way to do it on my own but curious what you guys think.
+def does_it_add_up(nums, target_num):
+  for r in range(1, len(nums) + 1):
+    for combo in combinations(nums, r):
+      if sum(combo) == target_num:
+        return True
+  return False
 
 while nums:
   die1 = random.randint(1, 6)
   die2 = random.randint(1, 6)
   rollvalue = die1 + die2
-  print("You rolled a " + str(rollvalue))
-  choice = input("Pick number to remove, separated by commas:")
-  choicelist = [i.strip() for i in choice.split(',')]
-  choicemath = 0
-  for i in choicelist:
-    choicemath += int(i)
-  if choicemath != rollvalue:
-    print("try again!")
+  #changes the word "a" to "an" in cases of rolling 8 or 11, for grammatical effect
+  if rollvalue == 8 or rollvalue == 11:
+    print("You rolled an " + str(rollvalue))
   else:
+    print("You rolled a " + str(rollvalue))
+
+  if does_it_add_up(nums, rollvalue):
+    choice = input("Pick number to remove, separated by commas:")
+    choicelist = [i.strip() for i in choice.split(',')]
+
+    choicemath = 0
     for i in choicelist:
-      if int(i) in nums:
-        nums.remove(int(i))
-      else:
-        print(str(i) + " is already gone")
+      choicemath += int(i)
+
+    if choicemath == rollvalue:
+      for i in choicelist:
+        if int(i) in nums:
+          nums.remove(int(i))
+    else:
+      print("Try again")
+      
+  else:
+    score = ""
+    for i in nums:
+      score += str(i)
+    print("You lost! Your score was " + (f"{int(score):,}")) #i looked up online how to format this to add commas into the score value, I don't really understand it.
+    break 
+    
+  
   print(nums)
-
-
-
