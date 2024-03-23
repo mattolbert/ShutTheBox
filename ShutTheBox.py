982bfbc3f8527951e6e832a0c076fcb188355e65
mattolbert <163608710+mattolbert@users.noreply.github.com>
2024-03-23 11:51:41 -0400

    what are good names to use for individual commits like this?

--- ShutTheBox.py
+++ ShutTheBox.py
@@ -20,7 +20,7 @@ def roll_die(num_dice):
     rollval = die1 + die2
   return rollval
 
-def has_duplicates(seq):#i built this because I accidentally typed "1, 1" in as user input and it did all sort of weirdness, I think this fixed it
+def has_duplicates(seq):
   return len(seq) != len(set(seq))
 
 while nums:
@@ -39,23 +39,25 @@ while nums:
     if_valid = False
     while if_valid == False:
       choice = input("Pick number(s) to remove, separated by commas:")
-      choicelist = [i.strip() for i in choice.split(',')] 
-      intlist = [int(i) for i in choicelist]#can this and the line above it all happen at once?
-      choicemath = sum(intlist)
+      choicelist = [int(i.strip()) for i in choice.split(',')] 
+      choicemath = sum(choicelist)
       
-      if choicemath == rollvalue:
-        if has_duplicates(intlist) == True:
-          print("You can't put the same number down twice")
-          break
-        else:
-          for i in intlist:
-            if i not in nums:
-              print("Your number choice has already been put down")
-              break       
-            nums.remove(i)
-            if_valid = True
-      else:
+      #Ensure the user input adds up to/matches the rolled dice value
+      if choicemath != rollvalue:
         print("Your choice doesn't match the roll value")
+      #Ensure the user didn't type the same number in twice
+      elif has_duplicates(choicelist) == True:
+        print("You can't put the same number down twice")
+      else:
+        # Ensure all requested tiles are still up
+        any_nums_down_already = any(i not in nums for i in choicelist)
+        if any_nums_down_already:
+            print("Your number choice has already been put down")
+        else:
+          #Good inputs. Knock down tiles
+          for i in choicelist:
+            nums.remove(i)
+          if_valid = True
 
     print(nums)    
 
@@ -63,7 +65,7 @@ while nums:
     score = ""
     for i in nums:
       score += str(i)
-    print(f"You lost. Good job, good effort! Your score was {int(score):,}")
+    print(f"You lost. Good job, good effort! There will come a day when you will successfully shut the box.... BUT IT IS NOT THIS DAY!\nYour score was {int(score):,}.")
     break 
 
 else:
