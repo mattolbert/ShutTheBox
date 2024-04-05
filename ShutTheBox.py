24ec21978b121913a148898384be2a25efb0b632
mattolbert <163608710+mattolbert@users.noreply.github.com>
2024-04-05 14:55:28 -0400

    Both_User_and_Comp_can_play
    -added option at the top for either the user to play or the comp to play
    -if comp, user sets how many games to sim
    -both options seem to run great I think.

--- ShutTheBox.py
+++ ShutTheBox.py
@@ -1,11 +1,18 @@
 import random
 from itertools import combinations
 
+player = int(input("If you'd like to play Shut the Box, press 1. If you'd like the computer to play, press 2:"))
+#i probably want input validation on both of these questions but don't fully understand how to do that yet
+how_many_plays = 1
+if player == 2:
+  how_many_plays = int(input("How many times would you like the computer to play?"))
+
 counter = 0
-score_list = []
-while counter < 10:
+stringscore_list = []
+while counter < how_many_plays:
   nums = list(range(1, 10))
-  print(nums)
+  if player == 1:
+    print(nums)
 
   def does_it_add_up(nums, target_num):
     combos = []
@@ -16,10 +23,10 @@ while counter < 10:
           combos.append(combo)
     #if the combos list isn't empty, play on
     if len(combos) > 0:
-      #print(combos)
       return True, combos
     #if the combos list is empty, you lost
-    return False, combos #i don't need to access combos once this is False but i get an error if the first return is a tuple and this isn't so I threw it in here too. Thoughts?
+    #i don't need to access combos once this is False but i get an error if the first return is a tuple and this isn't so I threw it in here too. Thoughts on this?
+    return False, combos 
 
   def roll_die(num_dice):
     if num_dice == '1':
@@ -35,69 +42,84 @@ while counter < 10:
 
   while nums:
     if 7 not in nums and 8 not in nums and 9 not in nums:
-      # dice_choice = input("Would you like to roll 1 die or 2? ")
-      rollvalue = roll_die('1')
+      if player == 1:
+        dice_choice = input("Would you like to roll 1 die or 2? ")
+        rollvalue = roll_die(dice_choice)
+      else:
+        rollvalue = roll_die('1')
     else:
       rollvalue = roll_die('2')
       
-    if rollvalue in [8,11]:
-      print(f"You rolled an {rollvalue}")
-    else:
-      print(f"You rolled a {rollvalue}")
+    if player == 1:
+      if rollvalue in [8,11]:
+        print(f"You rolled an {rollvalue}")
+      else:
+        print(f"You rolled a {rollvalue}")
 
-  #turn the returned tuple from the combo function above (true, list of combos) into a mutable list. i know i could just have it return automatically as a list but this felt more understandable in the moment.
+    #turn the returned tuple from the combo function above (true, list of combos) into a mutable list. i know i could just have it return automatically as a list but this felt more understandable in the moment.
     combo_function_list = does_it_add_up(nums, rollvalue)
 
     if combo_function_list[0] == True:
       if_valid = False
       while if_valid == False:
-        for i in combo_function_list[1][0]: #takes out the first option in the combo list
-          nums.remove(i)
-        if_valid = True
-        #if the number of the roll value is still available, put that number down automatically
-        # if rollvalue in nums:
-        #   nums.remove(rollvalue)
-        #   if_valid = True        
-        # else:
-        #   choice = input("Pick number(s) to remove, separated by commas:")
-        #   choicelist = [int(i.strip()) for i in choice.split(',')] 
-        #   choicemath = sum(choicelist)
+        if player == 2:
+          for i in combo_function_list[1][0]: #takes out the first option in the combo list
+            nums.remove(i)
+          if_valid = True
+        if player == 1:
+          choice = input("Pick number(s) to remove, separated by commas:")
+          choicelist = [int(i.strip()) for i in choice.split(',')] 
+          choicemath = sum(choicelist)
         
-        #   #Ensure the user input adds up to/matches the rolled dice value
-        #   if choicemath != rollvalue:
-        #     print("Your choice doesn't match the roll value")
-        #   #Ensure the user didn't type the same number in twice
-        #   elif has_duplicates(choicelist) == True:
-        #     print("You can't put the same number down twice")
-        #   else:
-        #     # Ensure all requested tiles are still up
-        #     any_nums_down_already = any(i not in nums for i in choicelist)
-        #     if any_nums_down_already:
-        #         print("Your number choice has already been put down")
-        #     else:
-        #       #Good inputs. Knock down tiles
-        #       for i in choicelist:
-        #         nums.remove(i)
-        #       if_valid = True
+          #Ensure the user input adds up to/matches the rolled dice value
+          if choicemath != rollvalue:
+            print("Your choice doesn't match the roll value")
+          #Ensure the user didn't type the same number in twice
+          elif has_duplicates(choicelist) == True:
+            print("You can't put the same number down twice")
+          else:
+            #Ensure all requested tiles are still up
+            any_nums_down_already = any(i not in nums for i in choicelist)
+            if any_nums_down_already:
+                print("Your number choice has already been put down")
+            else:
+              #Good inputs. Knock down tiles
+              for i in choicelist:
+                nums.remove(i)
+              if_valid = True
 
-      print(nums)    
+        if player == 1:
+          print(nums)    
 
     elif combo_function_list[0] == False:
-      score = ""
+      stringscore = ""
       for i in nums:
-        score += str(i)
-      print(f"You lost. Good job, good effort! There will come a day when you will successfully shut the box.... BUT IT IS NOT THIS DAY!\nYour score was {int(score):,}.")
-      break 
+        stringscore += str(i)
+      if player == 1:
+        print(f"You lost. Good job, good effort! There will come a day when you will successfully shut the box.... BUT IT IS NOT THIS DAY!\nYour score was {int(score):,}.")
+      break
 
   else:
-    print("You won! You shut the box!")
-    score = 0
-  
+    stringscore = '0'
+    if player == 1:
+      print("You won! You shut the box!")
+      
   counter += 1
-  score_list.append(score)
-print(score_list)
-wins = score_list.count(0)
-print(f"You played {counter} times and shut the box {wins} times! That's a win rate of {int(wins/counter*100)}%.")
+  #my list of scores were all strings like ('123', '45'), etc. but i wanted them to be ints so I could give some of the stats below
+  #I'm sure I could've done this more simply but chose this way
+  stringscore_list.append(stringscore)
+  score_list = [int(i) for i in stringscore_list]
+if player == 2:
+  print(score_list)
+  wins = score_list.count(0)
+  print(f"You played {counter} times and shut the box {wins} times! That's a win rate of {round((wins/counter*100), 2)}%.")
+  max_bad_score = score_list.count(max(score_list))
+  if max_bad_score > 1:
+    print(f"Your worst score was {max(score_list)} and you actually got that score {max_bad_score} times. Yikes!")
+  else:
+    print(f"Your worst score was {max(score_list)}. Yikes!")
+  if max(score_list) == 13456789:
+    print("Rolling snake eyes TWICE to start the game?! Ouch!")
   
 
 
